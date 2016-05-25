73d8313d9c029e79dd4f82864cc374562e1c44c0
root <born2net@gmail.com>
2016-05-25 11:33:01 -0700

    fixed OrderBy

--- src/pipes/OrderBy.ts
+++ src/pipes/OrderBy.ts
@@ -1,22 +1,21 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import * as _ from 'lodash';
-import {List} from 'immutable';
-import {StoreModel} from "../models/StoreModel";
-
-/** order by filter **/
 
 @Pipe({
     name: 'OrderBy'
 })
 export class OrderBy implements PipeTransform {
 
-    transform(input: Object[], [field, desc = false]: [string, boolean]): Object[] {
+    // transform(input: Object[], [field, desc = false]: [string, boolean]): Object[] {
+    transform(input: Object[], ...args:any[]): Object[] {
+        var field =  args[0];
+        var desc = args[1] == undefined ? false : args[1];
         if (input && field) {
-            return Array.from(input).sort((a: StoreModel, b: StoreModel) => {
-                if (a.getKey(field) < b.getKey(field)) {
+            return Array.from(input).sort((a: Object, b: Object) => {
+                if (_.get(a, field) < _.get(b, field)) {
                     return desc ? 1 : -1;
                 }
-                if (a.getKey(field) > b.getKey(field)) {
+                if (_.get(a, field) > _.get(b, field)) {
                     return desc ? -1 : 1;
                 }
                 return 0;
