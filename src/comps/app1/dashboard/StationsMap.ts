af728ef829947822798386e4d4671333f721810e
root <born2net@gmail.com>
2016-05-26 14:58:47 -0700

    google maps v2

--- src/comps/app1/dashboard/StationsMap.ts
+++ src/comps/app1/dashboard/StationsMap.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from "@angular/core";
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ChangeDetectorRef} from "@angular/core";
 import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from "angular2-google-maps/core";
 import {StationModel} from "../../../stations/StationModel";
 import * as _ from "lodash";
@@ -7,6 +7,7 @@ interface marker {
     id:number;
     lat:number;
     lng:number;
+    color:string;
     name:string;
     label?:string;
     draggable:boolean;
@@ -15,7 +16,6 @@ interface marker {
 @Component({
     selector: 'stationsMap',
     directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
-    changeDetection: ChangeDetectionStrategy.Default,
     template: `
         <sebm-google-map style="width: 100% ; height: 100%" 
               [latitude]="38.2500"
@@ -29,7 +29,7 @@ interface marker {
                   (markerClick)="clickedMarker(m, i)"
                   [latitude]="m.lat"
                   [longitude]="m.lng"
-                  [iconUrl]="getmarkerIcon(m)"
+                  [iconUrl]="getMarkerIcon(m)"
                   [label]="m.label">
                   
                 <!--<sebm-google-map-info-window>-->
@@ -41,6 +41,14 @@ interface marker {
     `
 })
 export class StationsMap {
+    constructor(private cdr:ChangeDetectorRef) {
+        setInterval(()=> {
+            this.cdr.reattach();
+            setTimeout(()=> {
+                this.cdr.detach();
+            }, 1000)
+        }, 3000)
+    }
 
     markers:marker[] = [];
     zoom:number = 4;
@@ -49,15 +57,14 @@ export class StationsMap {
     lat:number = 39.8282;
     lng:number = 98.5795;
 
-    getmarkerIcon(m:marker) {
-        return 'assets/screen_on.png';
+    getMarkerIcon(m:marker) {
+        this.cdr.detach();
+        return `assets/screen_${m.color}.png`;
     }
 
     clickedMarker(marker:marker, index:number) {
         this.onStationSelected.next(marker.id);
-        //console.log(`clicked the marker: ${label || index}`)
     }
-
 
     // mapClicked($event:MouseEvent) {
     //     this.markers.push({
@@ -92,63 +99,23 @@ export class StationsMap {
         return 'black';
     }
 
-
     private updateStations() {
         if (!this.m_stations)
             return;
-        // if (!this.highCharts)
-        //     return;
-        // var stations = [];
         var c = 0;
         this.m_stations.forEach((i_station:StationModel)=> {
             var geoLocation = i_station.getLocation();
             if (_.isEmpty(geoLocation))
                 return;
-
-            // this.markers.push({
-            //     lat: 51.673858,
-            //     lng: 7.815982,
-            //     label: 'A',
-            //     draggable: false
-            // })
-
-            function pinSymbol(color) {
-                return {
-                    path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
-                    fillColor: 'green',
-                    fillOpacity: 1,
-                    strokeColor: 'red',
-                    strokeWeight: 2,
-                    scale: 3,
-                };
-            }
-
-            var icon = {
-
-                path: "M-20,0a20,20 0 1,0 40,0a20,20 0 1,0 -40,0",
-                fillColor: '#000000',
-                fillOpacity: .6,
-                strokeWeight: 0,
-                scale: 3
-            }
-
             this.markers.push({
                 id: i_station.getStationId(),
                 name: i_station.getKey('name'),
+                color: i_station.getConnectionIcon('color'),
                 lat: +geoLocation.lat,
                 lng: +geoLocation.lon,
                 label: String(c++),
                 draggable: false
             })
-            // stations.push({
-            //     id: i_station.getStationId(),
-            //     name: i_station.getKey('name'),
-            //     publicIp: i_station.getPublicIp(),
-            //     lat: geoLocation.lat,
-            //     lon: geoLocation.lon,
-            //     color: i_station.getConnectionIcon('color')
-            // });
         });
-        // this.highCharts.series[1].setData(stations);
     }
 }
