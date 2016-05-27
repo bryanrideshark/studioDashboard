40a4c5c90ad0b85c82976812a5b4d3b792bd4bfa
root <born2net@gmail.com>
2016-05-27 14:52:15 -0700

    upd orders

--- jspm.browser.js
+++ jspm.browser.js
@@ -1,10 +1,11 @@
 SystemJS.config({
-  baseURL: "/",
-  paths: {
-    "github:*": "jspm_packages/github/*",
-    "npm:*": "jspm_packages/npm/*",
-    "rxjs/*": "node_modules/rxjs/*.js",
-    "src/*": "src/*",
-    "StudioDashboard/": "src/"
-  }
+    baseURL: "/",
+    production: false,
+    paths: {
+        "github:*": "jspm_packages/github/*",
+        "npm:*": "jspm_packages/npm/*",
+        "rxjs/*": "node_modules/rxjs/*.js",
+        "src/*": "src/*",
+        "StudioDashboard/": "src/"
+    }
 });
