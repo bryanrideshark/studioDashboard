79551026f17be5b1f8783f9f727ee93a72ec1443
root <born2net@gmail.com>
2016-06-04 16:25:27 -0700

    fix SimplelistEditor update change detection

---
+++ src/comps/app1/orders/OrderDetails.ts
@@ -0,0 +1,37 @@
+import {Component} from "@angular/core";
+import {BusinessAction} from "../../../business/BusinessAction";
+import {BusinessModel} from "../../../business/BusinessModel";
+import {AppStore} from "angular2-redux-util";
+import {List} from "immutable";
+import * as _ from 'lodash';
+
+@Component({
+    selector: 'orderDetails',
+    moduleId: __moduleName,
+    styleUrls: ['OrderDetails.css'],
+    templateUrl: 'OrderDetails.html'
+})
+
+export class OrderDetails {
+
+    constructor(private appStore:AppStore, private businessAction:BusinessAction) {
+        var i_businesses = this.appStore.getState().business;
+
+        this.businessesList = i_businesses.getIn(['businesses']);
+        this.unsub = this.appStore.sub((i_businesses:List<BusinessModel>) => {
+            this.businessesList = i_businesses;
+        }, 'business.businesses');
+
+    }
+
+    // @ViewChild(SimpleList)
+    // simpleList:SimpleList;
+
+    private unsub:Function;
+    private businessesList:List<BusinessModel> = List<BusinessModel>();
+
+    private ngOnDestroy() {
+        this.unsub();
+    }
+}
+
