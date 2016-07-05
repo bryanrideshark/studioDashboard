8097f46800577486b775b309328565c36bd46c64
root <born2net@gmail.com>
2016-07-04 21:32:36 -0700

    adnet customers model

--- src/adnet/AdnetReducer.ts
+++ src/adnet/AdnetReducer.ts
@@ -3,6 +3,7 @@ import * as AdnetActions from "./AdnetActions";
 
 export function adnet(state:Map<string,any> = Map<string,any>(), action:any):Map<string,any> {
     switch (action.type) {
+
         case AdnetActions.RECEIVE_CUSTOMERS:
         {
             return state.setIn(['customer'], action.customers);
