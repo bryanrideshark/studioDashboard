a1408848e7e79900054138e63b4e10f5751fccc6
root <born2net@gmail.com>
2016-07-04 21:27:26 -0700

    adnet customers model

--- src/adnet/AdnetReducer.ts
+++ src/adnet/AdnetReducer.ts
@@ -2,7 +2,6 @@ import {Map} from "immutable";
 import * as AdnetActions from "./AdnetActions";
 
 export function adnet(state:Map<string,any> = Map<string,any>(), action:any):Map<string,any> {
-
     switch (action.type) {
         case AdnetActions.RECEIVE_CUSTOMERS:
         {
