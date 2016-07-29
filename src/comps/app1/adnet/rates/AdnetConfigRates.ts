7d8b466a09781e69539cf6e1a053880c9add3bba
root <born2net@gmail.com>
2016-07-29 12:20:24 -0700

    basic grid working for rates

--- src/comps/app1/adnet/rates/AdnetConfigRates.ts
+++ src/comps/app1/adnet/rates/AdnetConfigRates.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectionStrategy, Input, ViewChild} from "@angular/core";
+import {Component, ChangeDetectionStrategy, Input, ViewChild, ElementRef} from "@angular/core";
 import {AppStore} from "angular2-redux-util";
 import {List} from "immutable";
 import * as _ from 'lodash';
@@ -7,20 +7,49 @@ import {AdnetRateModel} from "../../../../adnet/AdnetRateModel";
 
 @Component({
     selector: 'AdnetConfigRates',
+    styles: [`
+        .rateGridContainer {
+            overflow: auto;
+            width: 960px;
+            margin: auto;
+        }
+        
+        .square {
+            height: 50px;
+            width: 50px;
+            border: 1px solid;
+            display: block;
+            float: left;
+        
+        }
+        .new_row {
+            clear:both;
+        }
+        
+        .hover {
+            background-color: green;
+        }
+        `],
     moduleId: __moduleName,
     directives: [SimpleList],
-    templateUrl: 'AdnetConfigRates.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: 'AdnetConfigRates.html'
 })
 
 export class AdnetConfigRates {
 
-    constructor(private appStore: AppStore) {
+    constructor(private appStore: AppStore, private el: ElementRef) {
         var i_adnet = this.appStore.getState().adnet;
         this.rates = i_adnet.getIn(['rates']);
         this.unsub = this.appStore.sub((i_rates: List<AdnetRateModel>) => {
             this.rates = i_rates;
         }, 'adnet.rates');
+
+
+    }
+
+    ngOnInit() {
+        this.rateGridContainer = jQuery(this.el.nativeElement).find('.rateGridContainer');
+        this.makeGrid(8);
     }
 
     @Input()
@@ -30,29 +59,72 @@ export class AdnetConfigRates {
     }
 
     @ViewChild(SimpleList)
-    simpleList:SimpleList;
+    simpleList: SimpleList;
 
     private unsub: Function;
+    private rateGridContainer;
     private selectedAdnetCustomerId: string;
     private rates: List<AdnetRateModel> = List<AdnetRateModel>();
     private filteredRates: List<AdnetRateModel> = List<AdnetRateModel>();
 
+    private makeGrid(n) {
+        var hour: any = '';
+        var today: any = '';
+        var days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
+        for (var i = 1; i <= 25; i++) {
+            for (var j = 1; j <= 8; j++) {
+                if (j == 1) {
+                    hour = i - 1;
+                    if (hour == 0)
+                        hour = ''
+                } else {
+                    hour = ''
+                }
+                if (j > 1) {
+                    today = days.shift();
+                    if (!today)
+                        today = '';
+                }
+                if (today.length == 0 && hour.length == 0 && i != 1) {
+                    var cls = 'square';
+                } else {
+                    var cls = 'borderLessSquare';
+                }
+                if (_.isNumber(hour) && hour < 10) {
+                    hour = `0${hour}:00`
+                } else if (_.isNumber(hour) && hour > 9) {
+                    hour = `${hour}:00`
+                }
+                this.rateGridContainer.append(`<div class='${cls}'>${today}${hour}</div>`);
+                today = '';
+            }
+            this.rateGridContainer.append("<div class='new_row'></div>");
+        }
+
+        jQuery(document).on('click', '.square', function () {
+            jQuery(this).addClass('hover');
+        });
+        jQuery(document).on('mousedown', '.square', function () {
+            jQuery(this).addClass('hover');
+            // jQuery(this).removeClass('hover');
+        });
+    }
+
     private onSelection() {
         if (!this.filteredRates)
             return;
-        var selected:{} = this.simpleList.getSelected();
-        // var accountType = this.appStore.getState().appdb.get('accountType');
-        _.forEach(selected, (simpleItem:ISimpleListItem)=> {
+        var selected: {} = this.simpleList.getSelected();
+        _.forEach(selected, (simpleItem: ISimpleListItem)=> {
             if (simpleItem.selected) {
-                var adnetRateModel:AdnetRateModel = simpleItem.item;
+                var adnetRateModel: AdnetRateModel = simpleItem.item;
                 console.log(adnetRateModel.rateMap());
                 return;
             }
         })
     }
 
-    private updFilteredRates(){
-        if (this.rates && this.selectedAdnetCustomerId){
+    private updFilteredRates() {
+        if (this.rates && this.selectedAdnetCustomerId) {
             this.filteredRates = List<AdnetRateModel>();
             this.rates.forEach((i_adnetRateModel: AdnetRateModel)=> {
                 var adnetCustomerId = i_adnetRateModel.customerId();
