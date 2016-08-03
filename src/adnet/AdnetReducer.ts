e27ef265465b683f0c0293b223eff7b4f0081ca7
root <born2net@gmail.com>
2016-08-03 07:44:39 -0700

    saving ad rate table value!

--- src/adnet/AdnetReducer.ts
+++ src/adnet/AdnetReducer.ts
@@ -12,21 +12,28 @@ export function adnet(state: Map<string,any> = Map<string,any>(), action: any): 
             return state.setIn(['rates'], action.rates);
         }
         case AdnetActions.UPDATE_ADNET_RATE_TABLE: {
-            var rates:List<AdnetRateModel> = state.getIn(['rates']);
-            var indexOf = function(i_rateId:string) {
-                return rates.findIndex((i:AdnetRateModel) => i.rateId() === i_rateId);
+            var rates: List<AdnetRateModel> = state.getIn(['rates']);
+            var indexOf = function (i_rateId: string) {
+                return rates.findIndex((i: AdnetRateModel) => i.rateId() === i_rateId);
             }
-            rates = rates.update(indexOf(action.payload.rateId), (rate:AdnetRateModel) => {
+            rates = rates.update(indexOf(action.payload.rateId), (rate: AdnetRateModel) => {
                 return rate.setField('rateMap', action.payload.rateTable)
+            });
+            rates = rates.update(indexOf(action.payload.rateId), (rate: AdnetRateModel) => {
+                rate = rate.setField('hourRate0', action.payload.adHourlyRate["0"])
+                rate = rate.setField('hourRate1', action.payload.adHourlyRate["1"])
+                rate = rate.setField('hourRate2', action.payload.adHourlyRate["2"])
+                rate = rate.setField('hourRate3', action.payload.adHourlyRate["3"])
+                return rate;
             });
             return state.setIn(['rates'], rates);
         }
         case AdnetActions.UPDATE_ADNET_CUSTOMER: {
-            var customers:List<AdnetCustomerModel> = state.getIn(['customers'])
-            var indexOf = function(i_adnetCustomerId:string) {
-                return customers.findIndex((i:AdnetCustomerModel) => i.customerId() === i_adnetCustomerId);
+            var customers: List<AdnetCustomerModel> = state.getIn(['customers'])
+            var indexOf = function (i_adnetCustomerId: string) {
+                return customers.findIndex((i: AdnetCustomerModel) => i.customerId() === i_adnetCustomerId);
             }
-            customers = customers.update(indexOf(action.payload.Key), (customer:AdnetCustomerModel) => {
+            customers = customers.update(indexOf(action.payload.Key), (customer: AdnetCustomerModel) => {
                 return customer.setData<AdnetCustomerModel>(AdnetCustomerModel, action.payload)
             });
             return state.setIn(['customers'], customers);
