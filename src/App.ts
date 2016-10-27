effee3564335e051b605aab21c9eecde4afbedcc
root <born2net@gmail.com>
2016-10-27 08:49:31 -0700

    working on dropbox support v3

--- src/App.ts
+++ src/App.ts
@@ -151,7 +151,7 @@ import {InputString} from "./comps/inputstring/InputString";
 import {InputTextModule} from 'primeng/primeng';
 import {TreeModule} from 'angular2-tree-component';
 import {Dropbox} from "./comps/dropbox/Dropbox";
-// import {TreeModule} from 'primeng/primeng';
+import {TreeModule as TreeModulePrime} from 'primeng/primeng';
 
 
 
@@ -270,7 +270,7 @@ var providing = [CommBroker, AUTH_PROVIDERS,
 var decelerations = [Main, RatesTable, UsersDetails, LoginPanel, Menu, MenuItem, Account, Whitelabel, Apps, App1, Users, Adnet, Privileges, Dashboard, Logout, Orders, Filemenu, FilemenuItem, Logo, LogoCompany, Footer, BlurForwarder, InputEdit, OrderBy, SortBy, FilterPipe, AdnetConfigTargets, AdnetConfigRates, Tabs, Tab, ServerStats, ServerAvg, StationsMap, StationsGrid, StationDetails, ImgLoader, Ng2Highcharts, AdnetConfigCustomer, AdnetConfig, StationSnapshot, OrderDetails, SimpleList, PrivilegesDetails, ModalDialog, Infobox, UserStorage, Loading, Samplelist, UserInfo, AddUser, ChangePass, MODAL_DIRECTIVES, Ng2Highstocks, Ng2Highmaps, SimplelistEditable, AdnetConfigTargetStations, AdnetConfigTargetProps, AdnetLocation, MapAddress, AdnetNetwork, AdnetNetworkCustomerSelector, AdnetNetworkPackageEditor, AdnetNetworkPackageViewer, AdnetNetworkPackageTarget, AdnetNetworkPackageProps, AdnetNetworkPackageContent, AdnetNetworkPackageContentProps, AdnetNetworkTarget, AdnetNetworkTargetProps, ResourceViewer, AdnetNetworkPackageViewProps, AdnetNetworkPairProps, AdnetLoader, InputNumeric, InputString, Dropbox];
 
 @NgModule({
-    imports: [BrowserModule, SimpleGridModule.forRoot(), AgmCoreModule.forRoot(), JsonpModule, HttpModule, ReactiveFormsModule, FormsModule, DropdownModule, AccordionModule, routing, TreeModule, InputTextModule],
+    imports: [BrowserModule, SimpleGridModule.forRoot(), AgmCoreModule.forRoot(), JsonpModule, HttpModule, ReactiveFormsModule, FormsModule, DropdownModule, AccordionModule, routing, TreeModule, InputTextModule, TreeModulePrime],
     providers: [providing],
     declarations: decelerations,
     bootstrap: [Main],
--- src/comps/dropbox/Dropbox.ts
+++ src/comps/dropbox/Dropbox.ts
@@ -7,6 +7,8 @@ import {Lib} from "src/Lib";
 import {LocalStorage} from "../../services/LocalStorage";
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
+import * as _ from 'lodash';
+import {TreeNode} from 'primeng/primeng';
 
 @Component({
     selector: 'Dropbox',
@@ -18,7 +20,7 @@ import {Observable} from "rxjs/Observable";
      `],
     template: `
             <div style="width: 100%" class="btn-group" role="group">
-                <button (click)="renderTree()"  style="padding: 9px" type="button" class="btn btn-default">
+                <button (click)="refresh()"  style="padding: 9px" type="button" class="btn btn-default">
                   <span class="fa fa-refresh"></span>
                 </button>
                 <button  style="padding: 9px" type="button" class="b btn btn-default">
@@ -34,7 +36,7 @@ import {Observable} from "rxjs/Observable";
             <input class="form-control" style="width: 99.9%" type="password" (blur)="onTokenChange($event)" [(ngModel)]="token"/>
             <br/>
             <Tree [nodes]="nodes"></Tree>
-            <!--<p-tree [value]="files"></p-tree>-->
+            <p-tree [value]="nodes"></p-tree>
     `,
     moduleId: __moduleName
 })
@@ -53,6 +55,89 @@ export class Dropbox {
     private token;
     private accountValidity: boolean = false;
 
+    private refresh() {
+        console.log(this.nodes);
+        var a = JSON.stringify(this.nodes);
+        this.nodes = JSON.parse(a)
+        // this.nodes = [{
+        //     name: 'root1',
+        //     children: [{
+        //         name: 'child1'
+        //     }, {
+        //         name: 'child2'
+        //     }]
+        // }, {
+        //     name: 'root2',
+        //     children: [{
+        //         name: 'child2.1'
+        //     }, {
+        //         name: 'child2.2',
+        //         children: [{
+        //             name: 'subsub'
+        //         }]
+        //     }]
+        // }];
+        // this.nodes = [{
+        //     "name": "Camera Uploads",
+        //     "id": 0.847493314966749
+        // }, {
+        //     "name": "icons",
+        //     "id": 0.6112438279016996
+        // }, {
+        //     "name": "Public",
+        //     "id": 0.5698881344606346,
+        //     "children": []
+        // }, {
+        //     "name": "ScenesThumbnails",
+        //     "id": 0.5058557712692944,
+        //     "children": [{
+        //         "name": "Catalog",
+        //         "id": 0.6858089093372179
+        //     }, {
+        //         "name": "Clocks",
+        //         "id": 0.08159777717161387
+        //     }, {
+        //         "name": "Directory",
+        //         "id": 0.8477346312226623
+        //     }, {
+        //         "name": "Food Menu",
+        //         "id": 0.22730813226462
+        //     }, {
+        //         "name": "Generic",
+        //         "id": 0.870499072214203
+        //     }, {
+        //         "name": "Hospitality",
+        //         "id": 0.6575229856608689
+        //     }, {
+        //         "name": "Medical",
+        //         "id": 0.5275764594848011
+        //     }, {
+        //         "name": "Spa",
+        //         "id": 0.5354921093523624
+        //     }, {
+        //         "name": "Special Event",
+        //         "id": 0.42057998645209804
+        //     }, {
+        //         "name": "Sports",
+        //         "id": 0.7414530924893032
+        //     }, {
+        //         "name": "Stock",
+        //         "id": 0.035732473404250564
+        //     }, {
+        //         "name": "Transit",
+        //         "id": 0.17417703568476628
+        //     }, {
+        //         "name": "Weather",
+        //         "id": 0.4296740436130506
+        //     }]
+        // }, {
+        //     "name": "test",
+        //     "id": 0.35958166124184254,
+        //     "children": []
+        // }]
+        this.cd.markForCheck();
+    }
+
     private onTokenChange(event) {
         if (event.target.value.length < 20)
             return;
@@ -60,32 +145,17 @@ export class Dropbox {
         this.renderTree();
     }
 
-    private renderTree(i_folder: {} = '', i_clear: boolean = true) {
+    private renderTree(i_folder: {} = {name: ''}, i_start: boolean = true) {
         this.checkToken((status) => {
             if (!status)
                 return;
-            if (i_clear)
-                this.nodes = [];
+            var url;
+            if (i_folder['path']){
+                url = `https://secure.digitalsignage.com/DropboxFolders/${this.token}${i_folder['path']}`;
+            } else {
+                url = `https://secure.digitalsignage.com/DropboxFolders/${this.token}/${i_folder['name']}`;
+            }
 
-            // var getFilesInFolder = (i_folder:{})=>{
-            //     const url = `https://secure.digitalsignage.com/DropboxFiles/${this.token}/${i_folder}`;
-            //     return this._http.get(url)
-            //         .catch((err) => {
-            //             return Observable.throw(err);
-            //         })
-            //         .finally(() => {
-            //         })
-            //         .map((result: any) => {
-            //             var files: Array<string> = result.json();
-            //             i_folder['children'] = [];
-            //             i_folder['children'].push(files);
-            //             this.cd.markForCheck();
-            //         }).subscribe();
-            // }
-
-
-            console.log('loading data');
-            const url = `https://secure.digitalsignage.com/DropboxFolders/${this.token}/${i_folder}`;
             this._http.get(url)
                 .catch((err) => {
                     return Observable.throw(err);
@@ -93,14 +163,24 @@ export class Dropbox {
                 .finally(() => {
                 })
                 .map((result: any) => {
+                    if (i_start)
+                        this.nodes = [];
                     var folders: Array<string> = result.json();
-                    this.nodes = [];
-                    folders.forEach((folder,idx) => {
-                        var o = {name: folder}
-                        //name: folder.replace(/\//, '')
-                        this.nodes.push(o);
-                        // this.renderTree(o,false);
-                        // getFilesInFolder(folders[idx]);
+                    folders.forEach((folder, idx) => {
+                        var o = {
+                            name: folder.replace(/\//, ''),
+                            path: folder
+                        }
+                        if (i_start) {
+                            this.nodes.push(o);
+                        } else {
+                            if (!i_folder['children'])
+                                i_folder['children'] = [];
+                            var dirs = o.name.split('/')
+                            o.name = dirs[dirs.length - 1];
+                            i_folder['children'].push(o);
+                        }
+                        this.renderTree(o, false);
                     })
                     this.cd.markForCheck();
                 }).subscribe();
@@ -136,6 +216,7 @@ export class Dropbox {
 
     nodes = []
 
+
     // nodes = [{
     //     id: 1,
     //     name: 'root1',
@@ -163,57 +244,80 @@ export class Dropbox {
     // }];
 
 
-    // files: TreeNode[] = [
-    //     {
-    //         "data":
-    //             [
-    //                 {
-    //                     "label": "Documents",
-    //                     "data": "Documents Folder",
-    //                     "expandedIcon": "fa-folder-open",
-    //                     "collapsedIcon": "fa-folder",
-    //                     "children": [{
-    //                         "label": "Work",
-    //                         "data": "Work Folder",
-    //                         "expandedIcon": "fa-folder-open",
-    //                         "collapsedIcon": "fa-folder",
-    //                         "children": [{"label": "Expenses.doc", "icon": "fa-file-word-o", "data": "Expenses Document"}, {"label": "Resume.doc", "icon": "fa-file-word-o", "data": "Resume Document"}]
-    //                     },
-    //                         {
-    //                             "label": "Home",
-    //                             "data": "Home Folder",
-    //                             "expandedIcon": "fa-folder-open",
-    //                             "collapsedIcon": "fa-folder",
-    //                             "children": [{"label": "Invoices.txt", "icon": "fa-file-word-o", "data": "Invoices for this month"}]
-    //                         }]
-    //                 },
-    //                 {
-    //                     "label": "Pictures",
-    //                     "data": "Pictures Folder",
-    //                     "expandedIcon": "fa-folder-open",
-    //                     "collapsedIcon": "fa-folder",
-    //                     "children": [
-    //                         {"label": "barcelona.jpg", "icon": "fa-file-image-o", "data": "Barcelona Photo"},
-    //                         {"label": "logo.jpg", "icon": "fa-file-image-o", "data": "PrimeFaces Logo"},
-    //                         {"label": "primeui.png", "icon": "fa-file-image-o", "data": "PrimeUI Logo"}]
-    //                 },
-    //                 {
-    //                     "label": "Movies",
-    //                     "data": "Movies Folder",
-    //                     "expandedIcon": "fa-folder-open",
-    //                     "collapsedIcon": "fa-folder",
-    //                     "children": [{
-    //                         "label": "Al Pacino",
-    //                         "data": "Pacino Movies",
-    //                         "children": [{"label": "Scarface", "icon": "fa-file-video-o", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "fa-file-video-o", "data": "Serpico Movie"}]
-    //                     },
-    //                         {
-    //                             "label": "Robert De Niro",
-    //                             "data": "De Niro Movies",
-    //                             "children": [{"label": "Goodfellas", "icon": "fa-file-video-o", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "fa-file-video-o", "data": "Untouchables Movie"}]
-    //                         }]
-    //                 }
-    //             ]
-    //     }
-    // ]
+    files: TreeNode[] = [
+        {
+            "data": [
+                {
+                    "label": "Documents",
+                    "data": "Documents Folder",
+                    "expandedIcon": "fa-folder-open",
+                    "collapsedIcon": "fa-folder",
+                    "children": [{
+                        "label": "Work",
+                        "data": "Work Folder",
+                        "expandedIcon": "fa-folder-open",
+                        "collapsedIcon": "fa-folder",
+                        "children": [{
+                            "label": "Expenses.doc",
+                            "icon": "fa-file-word-o",
+                            "data": "Expenses Document"
+                        }, {
+                            "label": "Resume.doc",
+                            "icon": "fa-file-word-o",
+                            "data": "Resume Document"
+                        }]
+                    },
+                        {
+                            "label": "Home",
+                            "data": "Home Folder",
+                            "expandedIcon": "fa-folder-open",
+                            "collapsedIcon": "fa-folder",
+                            "children": [{
+                                "label": "Invoices.txt",
+                                "icon": "fa-file-word-o",
+                                "data": "Invoices for this month"
+                            }]
+                        }]
+                },
+                {
+                    "label": "Pictures",
+                    "data": "Pictures Folder",
+                    "expandedIcon": "fa-folder-open",
+                    "collapsedIcon": "fa-folder",
+                    "children": [
+                        {
+                            "label": "barcelona.jpg",
+                            "icon": "fa-file-image-o",
+                            "data": "Barcelona Photo"
+                        },
+                        {
+                            "label": "primeui.png",
+                            "icon": "fa-file-image-o",
+                            "data": "PrimeUI Logo"
+                        }]
+                }
+            ]
+        }
+    ]
 }
+// var getFilesInFolder = (i_folder:{})=>{
+//     if (!i_folder['name'])
+//         i_folder['name'] = '';
+//     const url = `https://secure.digitalsignage.com/DropboxFiles/${this.token}/${i_folder['name']}`;
+//     return this._http.get(url)
+//         .catch((err) => {
+//             return Observable.throw(err);
+//         })
+//         .finally(() => {
+//         })
+//         .map((result: any) => {
+//             var files: Array<string> = result.json();
+//             i_folder['children'] = [];
+//             i_folder['children'].push(files);
+//             this.cd.markForCheck();
+//         }).subscribe();
+// }
+
+// if (i_start==true)
+//     getFilesInFolder(this.nodes);
+//aa
