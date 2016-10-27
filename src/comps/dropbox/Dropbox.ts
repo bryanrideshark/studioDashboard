020baf57ade6e9eb91bbf3b6e1f4dd157a435f9d
root <born2net@gmail.com>
2016-10-27 06:13:40 -0700

    working on dropbox support

--- src/comps/dropbox/Dropbox.ts
+++ src/comps/dropbox/Dropbox.ts
@@ -1,12 +1,16 @@
 import {
     Component,
-    ChangeDetectionStrategy
+    ChangeDetectionStrategy,
+    ChangeDetectorRef
 } from "@angular/core";
 import {Lib} from "src/Lib";
 import {LocalStorage} from "../../services/LocalStorage";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
 @Component({
     selector: 'Dropbox',
+    changeDetection: ChangeDetectionStrategy.Default,
     styles: [`
         button {
         width: 33.3%;
@@ -14,19 +18,20 @@ import {LocalStorage} from "../../services/LocalStorage";
      `],
     template: `
             <div style="width: 100%" class="btn-group" role="group">
-                <button  style="padding: 9px" type="button" class="btn btn-default">
+                <button (click)="renderTree()"  style="padding: 9px" type="button" class="btn btn-default">
                   <span class="fa fa-refresh"></span>
                 </button>
                 <button  style="padding: 9px" type="button" class="b btn btn-default">
                   <span class="fa fa-minus"></span>
                 </button>
                 <button  style="padding: 9px" type="button" class=" btn btn-default">
-                  <span style="color: green" class="fa fa-check-square"></span>
+                  <span *ngIf="accountValidity" style="color: green" class="fa fa-check-square"></span>
+                  <span *ngIf="!accountValidity" style="color: red" class="fa fa-minus-square "></span>
                 </button>
                 <h2 style="display: inline; position: relative; top: -3px; left: 10px">{{totalFilteredPlayers}}</h2>
             </div>
             <br/>
-            <input class="form-control" style="width: 99.9%" type="password" (blur)="onToken($event)" [(ngModel)]="token"/>
+            <input class="form-control" style="width: 99.9%" type="password" (blur)="onTokenChange($event)" [(ngModel)]="token"/>
             <br/>
             <Tree [nodes]="nodes"></Tree>
             <!--<p-tree [value]="files"></p-tree>-->
@@ -36,54 +41,126 @@ import {LocalStorage} from "../../services/LocalStorage";
 
 
 export class Dropbox {
-    constructor(private localStorage: LocalStorage) {
+    constructor(private _http: Http, private localStorage: LocalStorage, private cd: ChangeDetectorRef) {
         this.me = Lib.GetCompSelector(this.constructor);
         if (this.localStorage.getItem('dropbox_key')) {
             this.token = this.localStorage.getItem('dropbox_key');
             this.renderTree();
         }
-
-
     }
 
     private me: string;
     private token;
+    private accountValidity: boolean = false;
 
-    private onToken(event) {
-        if (event.target.value.length > 20)
-            this.localStorage.setItem('dropbox_key', event.target.value);
+    private onTokenChange(event) {
+        if (event.target.value.length < 20)
+            return;
+        this.localStorage.setItem('dropbox_key', event.target.value);
+        this.renderTree();
     }
 
-    private renderTree() {
+    private renderTree(i_folder: {} = '', i_clear: boolean = true) {
+        this.checkToken((status) => {
+            if (!status)
+                return;
+            if (i_clear)
+                this.nodes = [];
 
+            // var getFilesInFolder = (i_folder:{})=>{
+            //     const url = `https://secure.digitalsignage.com/DropboxFiles/${this.token}/${i_folder}`;
+            //     return this._http.get(url)
+            //         .catch((err) => {
+            //             return Observable.throw(err);
+            //         })
+            //         .finally(() => {
+            //         })
+            //         .map((result: any) => {
+            //             var files: Array<string> = result.json();
+            //             i_folder['children'] = [];
+            //             i_folder['children'].push(files);
+            //             this.cd.markForCheck();
+            //         }).subscribe();
+            // }
+
+
+            console.log('loading data');
+            const url = `https://secure.digitalsignage.com/DropboxFolders/${this.token}/${i_folder}`;
+            this._http.get(url)
+                .catch((err) => {
+                    return Observable.throw(err);
+                })
+                .finally(() => {
+                })
+                .map((result: any) => {
+                    var folders: Array<string> = result.json();
+                    this.nodes = [];
+                    folders.forEach((folder,idx) => {
+                        var o = {name: folder}
+                        //name: folder.replace(/\//, '')
+                        this.nodes.push(o);
+                        // this.renderTree(o,false);
+                        // getFilesInFolder(folders[idx]);
+                    })
+                    this.cd.markForCheck();
+                }).subscribe();
+        });
     }
 
+    private checkToken(i_cb) {
+        const url = `https://secure.digitalsignage.com/DropboxAccountInfo/${this.token}`
+        this._http.get(url)
+            .catch((err) => {
+                this.updateAccountValidity(false);
+                i_cb(false);
+                return Observable.throw(err);
+            })
+            .finally(() => {
+            })
+            .map((result: any) => {
+                var reply: any = result.json();
+                if (reply.uid) {
+                    this.updateAccountValidity(true);
+                    i_cb(true);
+                } else {
+                    this.updateAccountValidity(false);
+                    i_cb(false);
+                }
+            }).subscribe();
+    }
 
-    nodes = [{
-        id: 1,
-        name: 'root1',
-        children: [{
-            id: 2,
-            name: 'child1'
-        }, {
-            id: 3,
-            name: 'child2'
-        }]
-    }, {
-        id: 4,
-        name: 'root2',
-        children: [{
-            id: 5,
-            name: 'child2.1'
-        }, {
-            id: 6,
-            name: 'child2.2',
-            children: [{
-                id: 7,
-                name: 'subsub'
-            }]
-        }]
-    }];
+    private updateAccountValidity(i_value) {
+        this.accountValidity = i_value;
+        this.cd.markForCheck();
+    }
+
+    nodes = []
+
+    // nodes = [{
+    //     id: 1,
+    //     name: 'root1',
+    //     children: [{
+    //         id: 2,
+    //         name: 'child1'
+    //     }, {
+    //         id: 3,
+    //         name: 'child2'
+    //     }]
+    // }, {
+    //     id: 4,
+    //     name: 'root2',
+    //     children: [{
+    //         id: 5,
+    //         name: 'child2.1'
+    //     }, {
+    //         id: 6,
+    //         name: 'child2.2',
+    //         children: [{
+    //             id: 7,
+    //             name: 'subsub'
+    //         }]
+    //     }]
+    // }];
 
 
     // files: TreeNode[] = [
