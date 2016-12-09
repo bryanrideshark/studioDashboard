892b3864fee40d6253db53540f2de1834242b0ff
root <born2net@gmail.com>
2016-12-09 09:26:14 -0800

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,43 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {AppComponent} from "./app.component";
-import "hammerjs";
-import {NgReduxModule} from "ng2-redux";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {AppStore} from "angular2-redux-util";
+import {applyMiddleware, createStore, compose, combineReducers} from "redux";
+import thunkMiddleware from 'redux-thunk';
+import 'hammerjs';
+import notify from '../appdb/NotifyReducer'
+import {LocalStorage} from "../services/LocalStorage";
+import {MsLibModule} from "ng-mslib/dist/mslib.module";
 import {ToastModule} from "ng2-toastr";
-import {ModalModule} from "ng2-bootstrap";
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+export enum ServerMode {
+    CLOUD,
+    PRIVATE,
+    HYBRID
+}
+
+
+var providing = [{
+    provide: AppStore, useFactory: () => {
+        const reducers = combineReducers({notify});
+        const middlewareEnhancer = applyMiddleware(<any>thunkMiddleware);
+        const isDebug = window['devToolsExtension']
+        const applyDevTools = () => isDebug ? window['devToolsExtension']() : f => f;
+        const enhancers: any = compose(middlewareEnhancer, applyDevTools());
+        const store = createStore(reducers, enhancers);
+        return new AppStore(store);
+    }, deps: []
+}, {
+    provide: "OFFLINE_ENV",
+    useValue: false
+}, {
+    provide: LocalStorage,
+    useClass: LocalStorage
+}];
+
 
 
 @NgModule({
@@ -17,16 +48,17 @@ import {ModalModule} from "ng2-bootstrap";
         BrowserModule,
         FormsModule,
         ReactiveFormsModule,
-        ModalModule,
         HttpModule,
         ToastModule,
-        NgReduxModule.forRoot()
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyAGD7EQugVG8Gq8X3vpyvkZCnW4E4HONLI'
+        }),
+        MsLibModule.forRoot()
     ],
-    providers: [],
+    providers: [providing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
-    constructor() {
+    constructor(i_app:AppStore) {
     }
 }
-
